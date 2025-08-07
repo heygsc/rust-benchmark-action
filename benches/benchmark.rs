af22251890c85bae7591bcef46b96182ac38bc55
heygsc <1596920983@qq.com>
2025-08-07 13:20:02 +0800

    feat: add short str

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,14 +1,24 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
 
 fn bench_replace(c: &mut Criterion) {
-    let s = "hello world".repeat(100_000);
-    
-    c.bench_function("replace", |b| {
+    let long_str = "hello world".repeat(100_000);
+    c.bench_function("long_str_100x_replace", |b| {
         b.iter(|| {
-            let mut s = black_box(s.clone());
+            let mut s = black_box(long_str.clone());
             for _ in 0..100 {
                 s = s.replace("hello", "hi");
+            }
+            s
+        })
+    });
+
+    let short_str = "a".repeat(40);
+    c.bench_function("short_str_100x_replace", |b| {
+        b.iter(|| {
+            let mut s = black_box(short_str.clone());
+            for _ in 0..100 {
+                s = s.replace("a", "b");
             }
             s
         })
